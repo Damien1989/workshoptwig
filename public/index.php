95c9b0974d7796859e8380d797088b57f55e2f82
Damien Diaz <contact.damiendiaz@gmail.com>
2021-10-20 17:16:34 +0200

    new

--- public/index.php
+++ public/index.php
@@ -2,8 +2,17 @@
 
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
-
-
+require '../vendor/autoload.php';
 $name = 'Wilder';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+
+
+$persons = [
+    ['name' => 'riri', 'age' => 10],
+    ['name' => 'fifi', 'age' => 20],
+    ['name' => 'loulou', 'age' => 30],
+];
+
+
+
+echo $twig->render('person.html.twig', ['persons' => $persons]);
